aa2a96e5fb22074b5c133489719f6ba7b8eee7ff
h3 <joaquinbastias321@gmail.com>
2022-07-26 02:32:41 -0300

    26/07/22

--- src/components/Pkmn.jsx
+++ src/components/Pkmn.jsx
@@ -1,29 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 const Pkmn = ({PkmnID='ditto'}) => {
     // if(!PkmnID) return;
     const url = 'https://pokeapi.co/api/v2/pokemon/';
-    let pkmnInfo = {
-        id: '',
-        name: ''
-    }
-    axios.get(url+PkmnID).then((el)=>{
-        pkmnInfo.id=el.data.id
-        pkmnInfo.name=el.data.name
+
+    const [pkmnInfo, setPkmnInfo] = useState(false);
+
+    if(!pkmnInfo){
+        axios.get(url+PkmnID).then((el)=>{
+            setPkmnInfo({
+                id: el.data.id,
+                name: el.data.name.toUpperCase()
+            })
+        })
         console.log(pkmnInfo)
-    })
+    }
 
     return (
         <tr>
             <td>{pkmnInfo.id}</td>
-            <td>foto</td>
+            <td></td>
             <td>{pkmnInfo.name}</td>
-            <td>tipo</td>
-            <td>tipo 2</td>
-            <td>peso</td>
+            <td></td>
+            <td></td>
+            <td></td>
         </tr>
     )
+
+    
     
 }
 
