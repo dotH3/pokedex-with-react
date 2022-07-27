09a0dab04d1801e62784a0545ff3a19d106f1764
h3 <joaquinbastias321@gmail.com>
2022-07-27 01:11:43 -0300

    27/07/22

--- src/components/Pkmn.jsx
+++ src/components/Pkmn.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
-const Pkmn = ({PkmnID='ditto'}) => {
+const Pkmn = ({PkmnID='1'}) => {
     // if(!PkmnID) return;
     const url = 'https://pokeapi.co/api/v2/pokemon/';
 
@@ -9,20 +9,36 @@ const Pkmn = ({PkmnID='ditto'}) => {
 
     if(!pkmnInfo){
         axios.get(url+PkmnID).then((el)=>{
+            const firstType = el.data.types[0].type.name;
+            var color = 'xd'
+            if(firstType=='water'){color='#84b6f4'}
+            if(firstType=='grass'){color='#77dd77'}
+            if(firstType=='fire'){color='#ff6961'}
+
+            var types = [firstType];
+            
+            if(el.data.types.length==2){types.push(el.data.types[1].type.name)}
+            types.push(color)
+
             setPkmnInfo({
                 id: el.data.id,
                 name: el.data.name.toUpperCase(),
-                img: el.data.sprites.front_default
+                img: el.data.sprites.front_default,
+                types,
             })
         })
-        console.log(pkmnInfo)
+        
     }
+
+    useEffect(()=>{
+        console.log(pkmnInfo.types.color)
+    })
 
     return (
         <tr>
             <td>{pkmnInfo.id}</td>
             <td>
-                <div className="bg-warning d-flex rounded-circle" style={{'width':'75px', 'height':'75px'}}>
+                <div className="d-flex rounded-circle" style={{'width':'75px', 'height':'75px'}}>
                     <img src={pkmnInfo.img} alt="" />
                 </div>
             </td>
